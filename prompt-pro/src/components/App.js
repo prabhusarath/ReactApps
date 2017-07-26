6fde7b1c2ca2c9f67012a91ba29e38468aa45379
prabhusarath <prabhu.sarath@gmail.com>
2017-07-25 20:43:48 -0500

    React Components

---
+++ prompt-pro/src/components/App.js
@@ -0,0 +1,24 @@
+import React,{ Component } from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="title"> Prompt Pro </div>
+                <div className="form-inline">
+                    <div className="form-group">
+                        <input className="form-control"
+                               placeholder="Task to do .."
+                        />
+                    </div>
+                    <button
+                        type="button"
+                        className="btn btn-success"> Add Task
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
