962d639bb2901cd740edd12e3eec59a05021a02d
prabhusarath <prabhu.sarath@gmail.com>
2017-07-26 12:29:23 -0500

    React Components

--- prompt-pro/src/components/App.js
+++ prompt-pro/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {addtask} from '../actions';
 import {bindActionCreators} from 'redux';
+import '../index.css';
 
 class App extends React.Component {
 
@@ -18,24 +19,44 @@ class App extends React.Component {
         this.props.addtask(this.state.text);
     }
 
+    renderReminders(){
+        const { reminders }  = this.props;
+
+        return (
+            <ul className="list-group col-sm-4">
+                {
+                    reminders.map( rem => {
+                        return (
+                            <li key={rem.id} className="list-group-item ">
+                                <div>{ rem.text }</div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+
 
     render() {
         return (
             <div className="App">
                 <div className="title"> Prompt Pro </div>
-                <div className="form-inline">
+                <div className="form-inline rem-form">
                     <div className="form-group">
                         <input className="form-control"
                                placeholder="Task to do .."
                                onChange={event => this.setState({text: event.target.value})}
                         />
                     </div>
+
                     <button
                         type="button"
                         className="btn btn-success"
                     onClick={() => this.logReminders()}> Add Task
                     </button>
                 </div>
+                { this.renderReminders() }
             </div>
         )
     }
